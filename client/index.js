4d88b73683c8bb3d642b9bb31d701efaa5ad45bc
Hadi Nasir <Hadinasser067@gmail.com>
2025-07-31 00:26:51 -0700

    Fix white screen issue: Remove duplicate App function in index.js

--- client/index.js
+++ client/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-function App() {
-  return <h1>Hello, Jokes App!</h1>;
-}
-
 // Create a root and render the app
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
